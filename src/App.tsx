6e56161d96a5d14f59f2376a008322d19562a0de
bel ✨ <le.bella98@gmail.com>
2025-09-07 11:24:33 -0700

    Sprites

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import PokemonBox from './components/PokemonBox'
 import PokemonSummary from './components/PokemonSummary'
